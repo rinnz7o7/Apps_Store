5fc962285eaa7d7d8586e496d26b50a52020cc14
Rinnz <rinnz1277@gmail.com>
2022-08-03 12:50:05 +0630

    first commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$('.owl-carousel').owlCarousel({
+     loop:true,
+     margin:15,
+     nav:true,
+     navText:["PREV","NEXT"],
+     responsive:{
+         0:{
+             items:1
+         },
+         576:{
+             items:2
+         },
+         768:{
+             items:3
+         },
+         992:{
+           items:3
+         },
+         1200:{
+           items:6
+         }
+   
+     }
+   })
